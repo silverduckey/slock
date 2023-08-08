a29f46cf48249d35a270b9728d695818a9842239
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-08-08 18:54:16 +0700

    Remove comments

--- config.def.h
+++ config.def.h
@@ -1,13 +1,11 @@
-/* user and group to drop privileges to */
 static const char *user = "vu";
 static const char *group = "vu";
 
 static const char *colorname[NUMCOLS] = {
-    [INIT] = "#2e3440",   /* after initialization */
-    [INPUT] = "#a3be8c",  /* during input */
-    [FAILED] = "#bf616a", /* wrong password */
-    [CAPS] = "#bf616a",   /* CapsLock on */
+    [INIT] = "#2e3440",
+    [INPUT] = "#a3be8c",
+    [FAILED] = "#bf616a",
+    [CAPS] = "#bf616a",
 };
 
-/* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
