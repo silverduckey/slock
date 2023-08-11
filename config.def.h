b374edc24fcc012222c941ac64190a2089656ea1
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-08-11 20:44:31 +0700

    Rewrite slock

--- config.def.h
+++ config.def.h
@@ -1,11 +1,13 @@
-static const char *user = "vu";
+/* user and group to drop privileges to */
+static const char *user  = "vu";
 static const char *group = "vu";
 
 static const char *colorname[NUMCOLS] = {
-    [INIT] = "#2e3440",
-    [INPUT] = "#a3be8c",
-    [FAILED] = "#bf616a",
-    [CAPS] = "#bf616a",
+	[INIT] =   "#2e3440",     /* after initialization */
+	[INPUT] =  "#a3be8c",   /* during input */
+	[FAILED] = "#bf616a",   /* wrong password */
+	[CAPS] = "#bf616a",         /* CapsLock on */
 };
 
+/* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
