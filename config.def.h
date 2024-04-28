5df964217f06a70206946dd71eedf75f386ba85f
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2024-04-28 16:23:03 +0700

    Update some colosr

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@ static const char *group = "vudangxyz";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "#1e1e2e",     /* after initialization */
-	[INPUT] =  "#a6e3a1",   /* during input */
+	[INPUT] =  "#89dceb",   /* during input */
 	[FAILED] = "#f38ba8",   /* wrong password */
 	[CAPS] = "#eba0ac",         /* CapsLock on */
 };
